be85492d5b985ffde849308b65d87aca66af3f4a
Michael Goldman <mikebgoldman95@gmail.com>
2025-04-17 17:48:17 +0200

    32-jdbc-autoconfig

--- 32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
+++ 32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
@@ -1,8 +1,16 @@
 package rewards;
 
+import config.RewardsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 // TODO-00 : In this lab, you are going to exercise the following:
 // - Understanding how auto-configuration is triggered in Spring Boot application
@@ -35,6 +43,9 @@ import org.springframework.boot.SpringApplication;
 // TODO-13 (Optional) : Follow the instruction in the lab document.
 //           The section titled "Build and Run using Command Line tools".
 
+@ConfigurationPropertiesScan
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@Import(RewardsConfig.class)
 public class RewardsApplication {
     static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
 
@@ -56,6 +67,24 @@ public class RewardsApplication {
     // - Use the JdbcTemplate bean that Spring Boot auto-configured for you
     // - Run this application and verify "Hello, there are 21 accounts" log message
     //   gets displayed in the console
+
+    @Bean
+    public CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate) {
+        return args -> {
+            // example: count "rows" in a T_ACCOUNT table from schemas
+            String sql = "SELECT COUNT(*) FROM T_ACCOUNT";
+            Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
+            logger.info("Number of accounts in T_ACCOUNT: {}", count);
+        };
+    }
+
+    // TO-DO-06: Implement a new command line runner that displays the name of the rewards recipient
+    @Bean
+    public CommandLineRunner commandLineRunner2(RewardsRecipientProperties rewardsRecipientProperties) {
+        return args -> {
+            logger.info("Recipient: {}", rewardsRecipientProperties.getName());
+        };
+    }
 
     // TODO-07 (Optional): Enable full debugging in order to observe how Spring Boot
     //           performs its auto-configuration logic
