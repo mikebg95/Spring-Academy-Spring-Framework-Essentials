be3408ca46769ffe3e96a1fb2fcc34bcbec79b05
Michael Goldman <mikebgoldman95@gmail.com>
2025-05-16 15:26:59 +0200

    42-security-rest

--- 42-security-rest/src/test/java/accounts/web/AccountControllerTests.java
+++ 42-security-rest/src/test/java/accounts/web/AccountControllerTests.java
@@ -46,7 +46,6 @@ public class AccountControllerTests {
     private AccountService accountService;
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"INVALID"})
     void accountSummary_with_invalid_role_should_return_403() throws Exception {
 
@@ -55,7 +54,6 @@ public class AccountControllerTests {
     }
 
     @Test
-    @Disabled
     @WithMockUser( roles = {"USER"})
     public void accountDetails_with_USER_role_should_return_200() throws Exception {
 
@@ -70,11 +68,9 @@ public class AccountControllerTests {
 
         // verify
         verify(accountManager).getAccount(0L);
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(username = "user", password = "user")
     public void accountDetails_with_user_credentials_should_return_200() throws Exception {
 
@@ -89,11 +85,9 @@ public class AccountControllerTests {
 
         // verify
         verify(accountManager).getAccount(0L);
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(username = "admin", password = "admin")
     public void accountDetails_with_admin_credentials_should_return_200() throws Exception {
 
@@ -108,7 +102,6 @@ public class AccountControllerTests {
 
         // verify
         verify(accountManager).getAccount(0L);
-
     }
 
     @Test
@@ -132,7 +125,6 @@ public class AccountControllerTests {
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"USER"})
     public void accountDetailsFail_test_with_USER_role_should_proceed_successfully() throws Exception {
 
@@ -143,11 +135,9 @@ public class AccountControllerTests {
                .andExpect(status().isNotFound());
 
         verify(accountManager).getAccount(any(Long.class));
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"ADMIN"})
     public void accountSummary_with_ADMIN_role_should_return_200() throws Exception {
 
@@ -161,11 +151,9 @@ public class AccountControllerTests {
                .andExpect(jsonPath("$..name").value("John Doe"));
 
         verify(accountManager).getAllAccounts();
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"ADMIN", "SUPERADMIN"})
     public void createAccount_with_ADMIN_or_SUPERADMIN_role_should_return_201() throws Exception {
 
@@ -180,7 +168,6 @@ public class AccountControllerTests {
                .andExpect(header().string("Location", "http://localhost/accounts/21"));
 
         verify(accountManager).save(any(Account.class));
-
     }
 
     // TODO-06b: Write a test that verifies that a user with "USER" role
@@ -191,14 +178,18 @@ public class AccountControllerTests {
     //    this testing because security failure will prevent
     //    calling a method of a dependency.)
     @Test
+    @WithMockUser(roles = {"USER"})
     public void createAccount_with_USER_role_should_return_403() throws Exception {
+        Account testAccount = new Account("1234512345", "Mary Jones");
+        testAccount.setEntityId(21L);
 
-
-
+        mockMvc.perform(post("/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(testAccount)))
+                .andExpect(status().isForbidden());
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"SUPERADMIN"})
     public void getBeneficiary_with_SUPERADMIN_role_should_return_200() throws Exception {
 
@@ -226,7 +217,6 @@ public class AccountControllerTests {
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"USER"})
     public void addBeneficiary_with_USER_role_should_return_403() throws Exception {
 
@@ -235,7 +225,6 @@ public class AccountControllerTests {
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"SUPERADMIN"})
     public void removeBeneficiary_with_SUPERADMIN_role_should_return_204() throws Exception {
 
@@ -247,11 +236,9 @@ public class AccountControllerTests {
                .andExpect(status().isNoContent());
 
         verify(accountManager).getAccount(0L);
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"USER", "ADMIN"})
     public void removeBeneficiary_with_USER_or_ADMIN_role_should_return_403() throws Exception {
 
@@ -265,7 +252,6 @@ public class AccountControllerTests {
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"SUPERADMIN"})
     public void removeBeneficiaryFail_test_with_SUPERADMIN_role_should_proceed_successfully() throws Exception {
         Account account = new Account("1234567890", "John Doe");
